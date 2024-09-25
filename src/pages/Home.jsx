bf78ef6ebb8d2f965ca8a86b770ffb7e35cb3288
Galih <galihrizkyfahrezy@gmail.com>
2024-09-25 09:29:02 +0700

    commit 008

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,22 +28,22 @@ export default function Home(){
 
             <div className="container mx-auto grid md:grid-cols-4 grid-cols-2 gap-3">
                 <a href="https://galihrf-sample-website-002.vercel.app/" target="blank">
-                    <Portofolio name='Sample Website 002' img='/portofolioSampleWeb002.png' desc='Sample home page with CSS, Tailwind, Javascript and React JS'/>
+                    <Portofolio name='Sample Website 002' img='/portofolioSampleWeb002.png' desc='Sample home page with CSS, Tailwind, Javascript and React JS.'/>
                 </a>
                 <a href="https://galihrizkyfahrezy.github.io/Sample_Website_001/" target="blank">
-                    <Portofolio name='Sample Website 001' img='/portofolioSampleWeb001.png' desc='Sample home page with CSS, Tailwind, Javascript and React JS'/>
+                    <Portofolio name='Sample Website 001' img='/portofolioSampleWeb001.png' desc='Sample home page with CSS, Tailwind, Javascript and React JS.'/>
                 </a>
                 <a href="https://galihrizkyfahrezy.github.io/weather_app/" target="_blank">
-                    <Portofolio name='Weather App' img='/portofolioWeatherApp.png' desc='Weather application with HTML, CSS and Javascript'/>
+                    <Portofolio name='Weather App' img='/portofolioWeatherApp.png' desc='Weather application with HTML, CSS and Javascript.'/>
                 </a>
                 <a href="https://galihrizkyfahrezy.github.io/To-Do_List/" target="_blank">
-                    <Portofolio name='To Do List' img='/portofolioToDoList.png' desc='To do list for your own list with HTML, CSS, Javascript'/>
+                    <Portofolio name='To Do List' img='/portofolioToDoList.png' desc='To do list for your own list with HTML, CSS, Javascript.'/>
                 </a>
                 <Link to={'/'}>
                     <Portofolio name='Game Review' img='/portofolioPortalGame.png' desc='Website Game Reviewer with CSS, Bootstrap, PHP and Laravel.'/>
                 </Link>
                 <Link to={'/'}>
-                    <Portofolio name='Portal Web' img='/portofolioSimpleCrud.png' desc='News Portal with PHP, Laravel and Bootstrap.'/>
+                    <Portofolio name='Portal Web' img='/portofolioSimpleCrud.png' desc='News Portal with CSS, Bootstrap, PHP Laravel.'/>
                 </Link>
             </div>
             <Footer/>
